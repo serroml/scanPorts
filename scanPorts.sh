7f71c3b1dbf4efc9e47eba01c80c18cc282c16f6
Sergio <115629650+serroml@users.noreply.github.com>
2023-01-30 14:17:17 +0100

    Update scanPorts.sh

--- scanPorts.sh
+++ scanPorts.sh
@@ -14,12 +14,12 @@ trap ctrl_c INT
 timeout 1 ping -c 1 $1 &>/dev/null
 
 
-if [[ $? -ne 0 ]]; then
+if (( $? != 0 )); then
 
         echo -e "\n[!] La maquina no es accesible \n \n"
         exit 2
 
-elif [[ $# -ne 1 ]]; then
+elif (( $# != 1 )); then
 
         echo -e "\n[!] Ayuda: bash $0 <Direccion IP/Nombre de dominio> \n \n"
         exit 3
